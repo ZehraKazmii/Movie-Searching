9d4145b7fea4f7b86c94cdd5794f3a49e6c428fb
ZehraKazmii <ayeshashahidkazmi6@gmail.com>
2024-09-11 07:42:49 +0100

    ASP.net

---
+++ WebApplication35/Controllers/HomeController1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json.Nodes;
+
+namespace WebApplication35.Controllers
+{
+    public class HomeController1 : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+            public IActionResult Index(string search)
+        {
+
+            string encodedSearch = Uri.EscapeDataString(search);
+
+            WebClient web = new WebClient();
+            string url = web.DownloadString($"https://www.omdbapi.com/?t={encodedSearch}&apikey=746026c4");
+            JsonNode data = JsonNode.Parse(url);
+            var neww = data["Year"];
+            var newe = data["Title"];
+            var newee = data["Actors"];
+            var new1 = data["Runtime"];
+            var new2 = data["Poster"];
+
+            ViewBag.y = neww;
+            ViewBag.z = newe;
+            ViewBag.x = newee;
+            ViewBag.a = new1;
+            ViewBag.b = new2;
+         
+            return View();
+           
+        }
+    }
+}
